9c59c11c690f418aaa314d8eb89677132e26a148
AbdulRehmanHameedullah <abdul@probedigital.co.uk>
2025-02-20 01:00:06 +0500

    firebase hosting integrated

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA6AU5MiicosQWto2PYi0uD3DnZs0zUVEc",
+  authDomain: "viorastore-ae620.firebaseapp.com",
+  projectId: "viorastore-ae620",
+  storageBucket: "viorastore-ae620.firebasestorage.app",
+  messagingSenderId: "277712350020",
+  appId: "1:277712350020:web:4da5edcb9ce2f75c574511",
+  measurementId: "G-GJFR4WGBZ3"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
